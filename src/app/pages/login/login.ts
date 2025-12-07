e89fc0c6147f1984820f20e6f2521279b257b875
Raul Waldimar <1000220231@unajma.edu.pe>
2025-12-07 08:49:50 -0500

    Cambiar ruta

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { AuthService } from '../../services/auth/auth';
+import { AuthService } from '../../services/auth';
 import { Router } from '@angular/router';
 
 @Component({
