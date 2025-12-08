b35ddba9953f45a216a07aa0ca26ec60156d8d98
Raul Waldimar <1000220231@unajma.edu.pe>
2025-12-08 07:02:26 -0500

    Agregando ruta curso en app.routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login';
 import { DashboardComponent } from './pages/dashboard/dashboard';
 import { authGuard } from './guards/auth.guard';
+import { CursosComponent } from './pages/cursos/cursos';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard] }
+  { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard] },
+  { path: 'cursos', component: CursosComponent }
 ];
--- src/app/pages/cursos/cursos.ts
+++ src/app/pages/cursos/cursos.ts
@@ -1,11 +1,41 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CourseService } from '../../services/course.service';
 
 @Component({
   selector: 'app-cursos',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './cursos.html',
-  styleUrl: './cursos.css',
+  styleUrls: ['./cursos.css']
 })
-export class Cursos {
+export class CursosComponent {
 
+  courseForm: FormGroup;
+  cursos: any[] = [];
+
+  constructor(
+    private fb: FormBuilder,
+    private courseService: CourseService
+  ) {
+    this.courseForm = this.fb.group({
+      nombre: ['', Validators.required],
+      docente: ['', Validators.required],
+      horario: ['', Validators.required],
+      cupos: ['', Validators.required],
+      semestre: ['', Validators.required]
+    });
+
+    this.courseService.getCourses().subscribe(data => {
+      this.cursos = data;
+    });
+  }
+
+  crearCurso() {
+    if (this.courseForm.invalid) return;
+
+    this.courseService.addCourse(this.courseForm.value);
+    this.courseForm.reset();
+  }
 }
